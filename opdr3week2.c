722eb8e5579a052d53382f410b8be3f072ae3b1e
Timo Stam <timo.stam2004@gmail.com>
2025-03-06 17:02:24 +0100

    e) klaar

--- opdr3week2.c
+++ opdr3week2.c
@@ -2,11 +2,15 @@
 #include <string.h>
 #define MAX_LENGTH 20
 
-int main(){
+int main() { 
     char buffer[MAX_LENGTH + 2];
     char key[MAX_LENGTH];
-    int value;
+    char value[MAX_LENGTH]; 
     int input = 0;
+
+    const char *fuels[] = {"benzine", "diesel", "LPG", "kerosine"};
+    int num_fuels = 4;
+
     while (!input) {
         printf("Stuur een key en een value die gesplitst zijn door een ':': ");
         fgets(buffer, sizeof(buffer), stdin);
@@ -14,12 +18,35 @@ int main(){
         if (len > 0 && buffer[len - 1] == '\n') {
             buffer[len - 1] = '\0';
         }
+
         if (strlen(buffer) > MAX_LENGTH) {
             printf("Teveel characters, het limiet is %d.\n", MAX_LENGTH);
         } else {
-            if (sscanf(buffer, "%[^:]: %d", key, &value) == 2) {
-                printf("Key: %s, Value: %d\n", key, value);
-                input = 1;
+            if (sscanf(buffer, "%[^:]: %s", key, value) == 2) {
+                if (strcmp(key, "Brandstof") == 0) {
+                    int valid = 0;
+                    for (int i = 0; i < num_fuels; i++) {
+                        if (strcmp(value, fuels[i]) == 0) {
+                            valid = 1;
+                            break;
+                        }
+                    }
+                    if (valid) {
+                        printf("Key: %s, Brandstof: %s\n", key, value);
+                        input = 1;
+                    } else {
+                        printf("Ongeldige brandstof: %s. Toegestane waardes: benzine, diesel, LPG, kerosine.\n", value);
+                    }
+                } 
+                else {
+                    int numericValue;
+                    if (sscanf(value, "%d", &numericValue) == 1) {
+                        printf("Key: %s, Value: %d\n", key, numericValue);
+                        input = 1;
+                    } else {
+                        printf("Foute invoer, probeer opnieuw.\n");
+                    }
+                }
             } else {
                 printf("Foute invoer, probeer opnieuw.\n");
             }
