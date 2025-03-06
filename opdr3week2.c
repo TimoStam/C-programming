44b954f5820a83a15a476e70dcd77d849068c782
Timo Stam <timo.stam2004@gmail.com>
2025-03-06 16:03:25 +0100

    d) klaar

--- opdr3week2.c
+++ opdr3week2.c
@@ -6,22 +6,24 @@ int main(){
     char buffer[MAX_LENGTH + 2];
     char key[MAX_LENGTH];
     int value;
-
-    printf("Stuur een key en een value die gesplitst zijn door een ':': ");
-    fgets(buffer, sizeof(buffer), stdin);
-    size_t len = strlen(buffer);
-    if (len > 0 && buffer[len - 1] == '\n') {
-        buffer[len - 1] = '\0';
-    }
-    if (strlen(buffer) > MAX_LENGTH) {
-        printf("Teveel characters, het limiet is %d.\n", MAX_LENGTH);
-    } else {
-        if (sscanf(buffer, "%[^:]: %d", key, &value) == 2) {
-            printf("Key: %s, Value: %d\n", key, value);
+    int input = 0;
+    while (!input) {
+        printf("Stuur een key en een value die gesplitst zijn door een ':': ");
+        fgets(buffer, sizeof(buffer), stdin);
+        size_t len = strlen(buffer);
+        if (len > 0 && buffer[len - 1] == '\n') {
+            buffer[len - 1] = '\0';
+        }
+        if (strlen(buffer) > MAX_LENGTH) {
+            printf("Teveel characters, het limiet is %d.\n", MAX_LENGTH);
         } else {
-            printf("Invalid input format.\n");
+            if (sscanf(buffer, "%[^:]: %d", key, &value) == 2) {
+                printf("Key: %s, Value: %d\n", key, value);
+                input = 1;
+            } else {
+                printf("Foute invoer, probeer opnieuw.\n");
+            }
         }
     }
-
     return 0;
 }
