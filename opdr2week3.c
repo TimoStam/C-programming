f75c77e40aee7874c705e99dff30560ee4f6a37c
Timo Stam <timo.stam2004@gmail.com>
2025-03-14 00:47:24 +0100

    finished 2b

--- opdr2week3.c
+++ opdr2week3.c
@@ -6,18 +6,39 @@ void generateArray(int low, int high, int size){
     srand(time(NULL));
     int r = 0;
     int *p1 = malloc(size * sizeof *p1);
+    int *p2 = malloc(size * sizeof *p2);
 
     if (p1 == NULL){
         printf("Failed to allocate memory");
         return;
     }
+
+    if (p2 == NULL){
+        printf("Failed to allocate memory");
+        return;
+    }
+
     for (int i = 0; i < size; i++){
         r = rand() % (high - low + 1) + low;
         p1[i] = r;
+    }
+
+    for (int i = 0; i < size; i++){
+        r = rand() % (high - low + 1) + low;
+        p2[i] = r;
+    }
+
+    for (int i = 0; i < size; i++){
         printf("%d ", p1[i]);
     }
     printf("\n");
+    for (int i = 0; i < size; i++){
+        printf("%d ", p2[i]);
+    }
+    printf("\n");
+    
     free(p1);
+    free(p2);
     return;
 }
 
