101e2de076f63409230f63760b43568361b3bf9c
Timo <timo.stam2004@gmail.com>
2025-03-14 00:22:14 +0100

    finished 2a

--- opdr2week3.c
+++ opdr2week3.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+void generateArray(int low, int high, int size){
+    srand(time(NULL));
+    int r = 0;
+    int *p1 = malloc(size * sizeof *p1);
+
+    if (p1 == NULL){
+        printf("Failed to allocate memory");
+        return;
+    }
+    for (int i = 0; i < size; i++){
+        r = rand() % (high - low + 1) + low;
+        p1[i] = r;
+        printf("%d ", p1[i]);
+    }
+    printf("\n");
+    free(p1);
+    return;
+}
 
 int main(){
-    float i = 0.1;
-    float j = 0.2;
-    float b = i+j;
-    printf("%.2f", b);
+    int lower = 0;
+    int higher = 0;
+    int size = 0;
+    scanf("%d %d %d", &lower, &higher, &size);
+    generateArray(lower, higher, size);
 }
