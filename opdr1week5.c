a1f58b08d4bb76977c058d9d843da981e2b56cbb
TimoStam <23099968@student.hhs.nl>
2025-03-31 16:23:31 +0200

    d working how tf does =+ compile

--- opdr1week5.c
+++ opdr1week5.c
@@ -41,6 +41,22 @@ void checkSumCol(int sudoku[9][9]){
     }    
 }
 
+void checkSumSquares(int sudoku[9][9]){
+    for (int blockRow =0; blockRow<9; blockRow +=3){
+        for (int blockCol =0; blockCol<9; blockCol +=3){
+            int sum = 0;
+            for (int row =blockRow; row<blockRow+3; row++){
+                for (int col =blockCol; col<blockCol+3; col++){
+                    sum += sudoku[row][col];
+                }
+            }
+            if (sum != 45){
+                printf("block at position %d %d does not equal 45 it equals %d\n", (blockRow/3)+1, (blockCol/3)+1, sum);
+            }
+        }
+    }
+}
+
 int main (){
     FILE *file = fopen("C:\\Users\\timos\\Documents\\CP\\C-programming\\sudoku.txt", "r");
     int sudoku[9][9];
@@ -53,6 +69,7 @@ int main (){
     printSudoku(sudoku);
     checkSumRow(sudoku);
     checkSumCol(sudoku);
+    checkSumSquares(sudoku);
 
 
 fclose(file);
