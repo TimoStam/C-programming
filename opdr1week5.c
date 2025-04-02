5cf877ac331f4d317dec8e7bf4f1da635a174b15
Timo <timo.stam2004@gmail.com>
2025-04-02 14:21:07 +0200

    changed code to use define instead of 9

--- opdr1week5.c
+++ opdr1week5.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 
+#define SIZE 9
+
 void fillSudoku(FILE *file, int sudoku[9][9]){
-    for (int row =0; row<9; row++){
-        for (int col = 0; col<9; col++){
+    for (int row =0; row<SIZE; row++){
+        for (int col = 0; col<SIZE; col++){
             fscanf(file, "%d", &sudoku[row][col]);
         }
     }
 }
 
-void printSudoku(int sudoku[9][9]){
-    for (int row =0; row<9; row++){
-        for (int col = 0; col<9; col++){
+void printSudoku(int sudoku[SIZE][SIZE]){
+    for (int row =0; row<SIZE; row++){
+        for (int col = 0; col<SIZE; col++){
             printf("%d ", sudoku[row][col]);
         }
         printf("\n");
     }
 }
 
-void checkSumRow(int sudoku[9][9]){
-    for (int row =0; row<9; row++){
+void checkSumRow(int sudoku[SIZE][SIZE]){
+    for (int row =0; row<SIZE; row++){
         int sum = 0;
-        for (int col = 0; col<9; col++){
+        for (int col = 0; col<SIZE; col++){
             sum += sudoku[row][col];
         }
         if (sum != 45){
@@ -29,10 +31,10 @@ void checkSumRow(int sudoku[9][9]){
     }
 }
 
-void checkSumCol(int sudoku[9][9]){
-    for (int col =0; col<9; col++){
+void checkSumCol(int sudoku[SIZE][SIZE]){
+    for (int col =0; col<SIZE; col++){
         int sum = 0;
-        for (int row = 0; row<9; row++){
+        for (int row = 0; row<SIZE; row++){
             sum += sudoku[row][col];
         }
         if (sum != 45){
@@ -41,9 +43,9 @@ void checkSumCol(int sudoku[9][9]){
     }    
 }
 
-void checkSumSquares(int sudoku[9][9]){
-    for (int blockRow =0; blockRow<9; blockRow +=3){
-        for (int blockCol =0; blockCol<9; blockCol +=3){
+void checkSumSquares(int sudoku[SIZE][SIZE]){
+    for (int blockRow =0; blockRow<SIZE; blockRow +=3){
+        for (int blockCol =0; blockCol<SIZE; blockCol +=3){
             int sum = 0;
             for (int row =blockRow; row<blockRow+3; row++){
                 for (int col =blockCol; col<blockCol+3; col++){
@@ -59,7 +61,7 @@ void checkSumSquares(int sudoku[9][9]){
 
 int main (){
     FILE *file = fopen("C:\\Users\\timos\\Documents\\CP\\C-programming\\sudoku.txt", "r");
-    int sudoku[9][9];
+    int sudoku[SIZE][SIZE];
     if(file == NULL) {
         printf("Not able to open the file.");
         return 1;
