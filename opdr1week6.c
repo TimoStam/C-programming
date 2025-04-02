6b0ff7ed48b9d7e6b3e95392dfed23a8f902b445
TimoStam <23099968@student.hhs.nl>
2025-04-02 22:40:19 +0200

    ig i did 1f instead of 1e this keeps happening help

--- opdr1week6.c
+++ opdr1week6.c
@@ -10,23 +10,39 @@ typedef struct {
 
 }Course;
 
+void changeCourse(Course* course[]){
+    int input, index = 0;
+    do {
+        printf("What course do you want to change: ");
+        input = scanf_s("%d", &index);
+        if (input != 1 || index <-1 || index >=MAX_COURSES){
+            printf("Try again\n");
+            while (getchar() != '\n');
+        }
+    } while (input !=1 || index <-1 || index >=MAX_COURSES);
+    printf("%d\n", index);
+
+    printf("What would the new course be: ");
+    scanf_s("%19s", course[index]->name, 20);
+}
 
 void main (){
     Course* courses[MAX_COURSES];
-    Course courses1[MAX_COURSES];
-    printf("%d\n", sizeof(courses1));
     for (int i =0; i<MAX_COURSES; i++){
         courses[i] = malloc(sizeof(Course));
     }
-    printf("%d\n", sizeof(courses));
     int courseCount = 0;
     char courseName[20];
     while (courseCount<MAX_COURSES && scanf_s("%19s", courses[courseCount]->name, 20) == 1){
         printf("Entered course: %19s\n", courses[courseCount]->name);
         courseCount++;
     }
-    
-
+    courseCount = 0;
+    changeCourse(courses);
+    while (courseCount<MAX_COURSES){
+        printf("courses %19s\n", courses[courseCount]->name);
+        courseCount++;
+    }
     for (int i =0; i<MAX_COURSES; i++){
         free(courses[i]);
     }
