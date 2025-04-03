0aa33f18ce4156a55cdffb3e24e61eedb9802752
TimoStam <23099968@student.hhs.nl>
2025-04-03 06:28:50 +0200

    2b finished ??

--- opdr2week7.c
+++ opdr2week7.c
@@ -6,8 +6,15 @@ double fahrenheitToCelsius(double fahrenheit){
     return (fahrenheit - 32)*5/9;
 }
 
+double voltageToCelsius(double voltage){
+    return (voltage*20)-20;
+}
+
 void main(){
     double fahrenheit = 80.0;
-    double celsius = fahrenheitToCelsius(fahrenheit);
-    printf("%.1f", celsius);
+    double tempCelsius = fahrenheitToCelsius(fahrenheit);
+    printf("%.1f\n", tempCelsius);
+    double voltage = 2.1;
+    double sensCelsius = voltageToCelsius(voltage);
+    printf("%.1f\n", sensCelsius);
 }
