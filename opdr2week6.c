d89e0862cc252798f512665c4d2e3db12d6d00b0
TimoStam <23099968@student.hhs.nl>
2025-04-03 02:56:48 +0200

    finished 2b

--- opdr2week6.c
+++ opdr2week6.c
@@ -8,33 +8,71 @@ typedef struct node{
     struct node *next;
 }node_t;
 
-void linkedList(int numbers[]){
-    int count;
-    node_t *head = NULL;
-    node_t *current = head;
+void printList(node_t **headRef){
+    node_t *current = *headRef;
 
-    for (count = 0; count<END; count++){
-        node_t *newNode = (node_t *) malloc(sizeof(node_t));
-        newNode->data = numbers[count];
-        newNode->next = NULL;
-
-        if (head == NULL){
-            head = newNode;
-            current = head;
-        } else {
-            current->next = newNode;
-            current = newNode;
-        }
-    }
-    current = head;
-
+    printf("Linked list: \n");
     while (current != NULL){
         printf("%d\n", current->data);
         current = current->next;
     }
 }
 
+void removeNode(node_t **headRef){
+    node_t *current = *headRef;
+    node_t *temp = *headRef;
+    int input, index = 0;
+    do {
+        printf("What node do you want to remove: \n");
+        input = scanf_s("%d", &index);
+        if (input != 1 || index < 0 || index >= END){
+            printf("Try again\n");
+            while (getchar() != '\n');
+        }
+    } while (input != 1 || index < 0 || index >= END);
+
+    if (*headRef == NULL){
+        return;
+    }
+    for (int i =0; i<index-1; i++){
+        if (current->next == NULL){
+            return;
+        }
+        current = current->next;
+    }
+    if (current->next == NULL){
+        return;
+    }
+    temp = current->next;
+    int val = temp->data;
+    printf("deleted value: %d\n", val);
+    current->next = temp->next;
+    free(temp);
+}
+
+void linkedList(int numbers[], node_t **headRef){
+    int count;
+    node_t *current = *headRef;
+
+    for (count = 0; count<END; count++){
+        node_t *newNode = (node_t *) malloc(sizeof(node_t));
+        newNode->data = numbers[count];
+        newNode->next = NULL;
+
+        if (*headRef == NULL){
+            *headRef = newNode;
+            current = *headRef;
+        } else {
+            current->next = newNode;
+            current = newNode;
+        }
+    }
+    current = *headRef;
+}
+
 void main(){
+    node_t *head = NULL;
+
     int numbers[END] = {0,1,2,3,4};
     int count = 0;
     printf("Array of numbers: \n");
@@ -42,8 +80,10 @@ void main(){
         printf("%d\n", numbers[count]);
         count++;
     }
-    count = 0;
 
-    printf("Linked list: \n");
-    linkedList(numbers);
+    linkedList(numbers, &head);
+    printList(&head);
+    removeNode(&head);
+    printList(&head);
+
 }
