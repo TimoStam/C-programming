2a25d8c62db0ba47744845ddeb0ebe9f4c855270
Timo <timo.stam2004@gmail.com>
2025-04-03 11:42:43 +0200

    i changed stuff ig

--- opdr2week7.c
+++ opdr2week7.c
@@ -2,19 +2,42 @@
 #include <stdlib.h>
 #include <string.h>
 
-double fahrenheitToCelsius(double fahrenheit){
+#define MAX_VALUES 20
+
+float fahrenheitToCelsius(float fahrenheit){
     return (fahrenheit - 32)*5/9;
+    return 0;
 }
 
-double voltageToCelsius(double voltage){
+float voltageToCelsius(float voltage){
     return (voltage*20)-20;
 }
 
-void main(){
-    double fahrenheit = 80.0;
-    double tempCelsius = fahrenheitToCelsius(fahrenheit);
-    printf("%.1f\n", tempCelsius);
-    double voltage = 2.1;
-    double sensCelsius = voltageToCelsius(voltage);
-    printf("%.1f\n", sensCelsius);
+void copyWithConversion(float data[], int size, float (*conversion)(float)){
+    for (int i =0; i<size; i++){
+        printf("data: %.2f, converted: %.2f\n", data[i], conversion(data[i]));
+    }
+}
+int main(){
+    FILE *file1;
+    FILE *file2;
+    file1 = fopen("C:\\Users\\timos\\Documents\\C programming\\fahrenheit.txt", "r");
+    file2 = fopen("C:\\Users\\timos\\Documents\\C programming\\voltage.txt", "r");
+    float fdata[MAX_VALUES];
+    float vdata[MAX_VALUES];
+    int count = 0;
+    while (count<MAX_VALUES && fscanf(file1, "%f", &fdata[count]) == 1){
+        count++;
+    }
+    count = 0;
+    while (count<MAX_VALUES && fscanf(file2, "%f", &vdata[count]) == 1){
+        count++;
+    }
+    fclose(file1);
+    fclose(file2);
+    printf("Fahrenheit to celsius conversion: \n");
+    copyWithConversion(fdata, MAX_VALUES, fahrenheitToCelsius);
+    printf("Voltage to celsius conversion: \n");
+    copyWithConversion(vdata, MAX_VALUES, voltageToCelsius);
+    return 0;
 }
