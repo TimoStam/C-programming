53c8182792a3e1267a9fccb1efa2c533283cbeb9
TimoStam <23099968@student.hhs.nl>
2025-04-03 06:05:41 +0200

    finished 1c

--- opdr1week7.c
+++ opdr1week7.c
@@ -16,6 +16,19 @@ int compareCourseName(const void *a, const void *b){
     return strcmp(resultA->course, resultB->course);
 }
 
+int compareECTS(const void *a, const void *b){
+    Results *resultA = (Results *)a;
+    Results *resultB = (Results *)b;
+
+    if (resultA->ECTS > resultB->ECTS){
+        return 1;
+    }
+    if (resultA->ECTS < resultB->ECTS){
+        return -1;
+    }
+    return 0;
+}
+
 void main(){
     Results results[] = {
         {1, "math", 9, 4.0}, 
@@ -24,7 +37,21 @@ void main(){
         {4, "chemistry", 15, 9.0}
     };
     qsort(results, 4, sizeof(Results), compareCourseName);
+    printf("Sorted by course name: \n");
     for(int i =0; i<4; i++){
-        printf("%s\n", results[i].course);
+        printf("id: %d, course: %s, ECTS: %d, grade: %.1f\n",
+            results[i].id, 
+            results[i].course,
+            results[i].ECTS,
+            results[i].grade);
+    }
+    qsort(results, 4, sizeof(Results), compareECTS);
+    printf("Sorted by ECTS count: \n");
+    for(int i =0; i<4; i++){
+        printf("id: %d, course: %s, ECTS: %d, grade: %.1f\n",
+            results[i].id, 
+            results[i].course,
+            results[i].ECTS,
+            results[i].grade);
     }
 }
