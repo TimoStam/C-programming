0eaf91f99759e800feed0adf59c65b97fba7b7d4
TimoStam <23099968@student.hhs.nl>
2026-02-12 11:22:50 +0100

    Cleanup

--- week1opdr4.c
+++ week1opdr4.c
@@ -12,27 +12,30 @@ int main(){
     int turns = 10;
     for (int i = 0; i < turns; i++){
         for (int j = 0; i < player_count; i++){
-            if (scanf("%d", current_throw) != 0 && current_throw >= 0 && current_throw <= 10){
-                if (0 == j){
-                    temp = current_throw;
-                    if (10 == temp){
-                        printf("Strike\n");
-                        pins1[i] = temp;
-                        temp = 0;
-                        break;
+            for (int k = 0; k < throws; k++){
+                if (scanf("%d", current_throw) != 0 && current_throw >= 0 && current_throw <= 10){
+                    if (0 == j){
+                        temp += current_throw;
+                        if (10 == temp){
+                            printf("Strike\n");
+                            pins1[i] = temp;
+                            temp = 0;
+                            break;
+                        }
+                    }
+                    if (1 == j){
+                        temp += current_throw;
+                        if (10 == temp){
+                            printf("Strike\n");
+                            pins2[i] = temp;
+                            temp = 0;
+                            break;
                     }
                 }
-                if (1 == j){
-                    temp = current_throw;
-                    if (10 == temp){
-                        printf("Strike\n");
-                        pins2[i] = temp;
-                        temp = 0;
-                        break;
-                }
-            }
-            if (10 == turns){
+                if (10 == turns){
 
+                }
+                }
             }
         }
     }
